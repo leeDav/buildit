02ac4048e9f9d334b632d591c29fd2853c3954b6
Lee Davies <lee@atomicStructure.net>
2017-06-26 14:35:03 +0100

    Added e2e test
    Only one, but it's a start

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,19 +1,16 @@
-// // For authoring Nightwatch tests, see
-// // http://nightwatchjs.org/guide#usage
-//
-// module.exports = {
-//   'default e2e tests': function test(browser) {
-//     // automatically uses dev Server port from /config.index.js
-//     // default: http://localhost:8080
-//     // see nightwatch.conf.js
-//     const devServer = browser.globals.devServerURL;
-//
-//     browser
-//       .url(devServer)
-//       .waitForElementVisible('#app', 5000)
-//       .assert.elementPresent('.hello')
-//       .assert.containsText('h1', 'Welcome to Your Vue.js App')
-//       .assert.elementCount('img', 1)
-//       .end();
-//   },
-// };
+module.exports = {
+  'default e2e tests': function test(browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL;
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.weather__card')
+      .assert.containsText('.weather__card-time', '00:00')
+      .assert.elementCount('.weather__card', 40)
+      .end();
+  },
+};
